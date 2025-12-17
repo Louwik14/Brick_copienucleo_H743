30a9c7c52a4340865b4066d04e87e897043a63c1
Louwik14 <loicdu14150@hotmail.fr>
2025-12-17 23:09:28 +0100

    Make SPI5 bus init safe and avoid duplicate display init

--- drivers/spi5_bus.c
+++ drivers/spi5_bus.c
@@ -6,10 +6,12 @@ mutex_t spi5_mutex;
 static bool spi5_initialized = false;
 
 void spi5_bus_init(void) {
-    if (spi5_initialized) {
-        return;
+    chSysLock();
+
+    if (!spi5_initialized) {
+        chMtxObjectInit(&spi5_mutex);
+        spi5_initialized = true;
     }
 
-    chMtxObjectInit(&spi5_mutex);
-    spi5_initialized = true;
+    chSysUnlock();
 }
